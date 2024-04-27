b07a75e481d6114b24e8d7d95cc1859c08ffd544
gdrosos <t8180024@aueb.gr>
2024-04-27 23:49:36 +0300

    Polish readme

--- README.md
+++ README.md
@@ -147,10 +147,12 @@ Each key in the JSON object represents a project, and the values are the release
 For a quicker resolution process, we give the option to resolve dependencies for a subset of the dataset.
 This process handles only 50 projects and is significantly faster. To execute this, run:
 ```bash
-sh scripts/dependency_resolution/run_dep_resolution.sh <your_github_token> data subset
+sh scripts/dependency_resolution/run_dep_resolution.sh <your_github_token> data/subset subset
 ```
 This execution will download the source code of 50 projects
-and generate a `project_dependencies_subset.json` file containing the resolved dependencies for the subset of projects.
+and generate a `project_dependencies_subset.json` file in the `data/subset` directory containing the resolved dependencies for the subset of projects.
+
+### Partial Call Graph Construction (Section 2.3.1)
 
 
 
--- scripts/dependency_resolution/run_dep_resolution.sh
+++ scripts/dependency_resolution/run_dep_resolution.sh
@@ -4,19 +4,19 @@
 GITHUB_TOKEN=$1
 DEST_DIR=$2
 SUBSET=$3
-
+cd scripts/dependency_resolution
 if [ "$SUBSET" = "subset" ]; then
     echo "Processing sample dataset..."
-    python3 clone_repos.py -t $GITHUB_TOKEN  -f ../../data/sample_projects.csv -d $DEST_DIR
-    python3.9 dependency_resolver.py -f ../../data/sample_projects.csv -d $DEST_DIR
-    python3.9 merge_json.py -d $DEST_DIR -o project_dependencies_subset.json
+    python3 clone_repos.py -t $GITHUB_TOKEN  -f ../../$DEST_DIR/sample_projects.csv -d ../../$DEST_DIR/sources/apps
+    python3.9 dependency_resolver.py -f ../../$DEST_DIR/sample_projects.csv -d ../../$DEST_DIR/sources/apps
+    python3.9 merge_json.py -d ../../$DEST_DIR -o ../../$DEST_DIR/project_dependencies_subset.json
 else
     echo "Processing full dataset..."
     wget -O dataset.zip https://zenodo.org/records/5645517/files/dataset-package.zip?download=1
     unzip dataset.zip 
-    awk -F"," 'NR > 1 {print $1}' dataset-package/python-projects-dataset.csv  > python_projects.csv
-    python3 clone_repos.py -t $GITHUB_TOKEN  -f python_projects.csv -d $DEST_DIR
-    python3.9 dependency_resolver.py -f python_projects.csv -d $DEST_DIR
-    python3.9 merge_json.py -d $DEST_DIR -o project_dependencies.json
+    awk -F"," 'NR > 1 {print $1}' dataset-package/python-projects-dataset.csv  > ../../$DEST_DIR/python_projects.csv
+    python3 clone_repos.py -t $GITHUB_TOKEN  -f ../../$DEST_DIR/python_projects.csv -d ../../$DEST_DIR
+    python3.9 dependency_resolver.py -f ../../$DEST_DIR/python_projects.csv -d ../../$DEST_DIR
+    python3.9 merge_json.py -d ../../$DEST_DIR -o ../../$DEST_DIR/project_dependencies.json
 fi
 echo "Dependency resolution completed."
